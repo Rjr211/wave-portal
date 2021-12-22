59ef7c1e4d860c3fae4cc637ee2572f1acb68a44
Rob <robert.j1450@gmail.com>
2021-12-22 12:07:07 -0500

    Updated wave contract + added sender + called wave function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,7 +8,19 @@ import "hardhat/console.sol";
 
 //Create initial contract
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
-        console.log("I am a contract");
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
---
+++ scripts/run.js
@@ -0,0 +1,42 @@
+
+
+const main = async () => {
+const [owner, randomPerson] = await hre.ethers.getSigners();
+
+
+
+//Compiles our contract and generrate the necesary files to work with our contract Under the artifacts directory
+const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+
+//Hardhat will create a local Ethereum network, only for this network.
+//After completion it'll destroy that local ntwrk. Everytime it runs it'll be on a fresh blockchain
+const waveContract = await waveContractFactory.deploy();
+
+//Waits until our contract is deployed to local blockchain. Constructor runs when we actually deploy
+await waveContract.deployed();
+
+//Logs the address of the deployed contract. How we find our contract on the blockchain
+console.log("Contract deployed to:", waveContract.address);
+console.log("Contract deployed by:", owner.address);
+
+let waveCount;
+waveCount = await waveContract.getTotalWaves();
+
+let waveTxn = await waveContract.wave();
+await waveTxn.wait();
+
+waveCount = await waveContract.getTotalWaves();
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    }
+    catch (error){
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
